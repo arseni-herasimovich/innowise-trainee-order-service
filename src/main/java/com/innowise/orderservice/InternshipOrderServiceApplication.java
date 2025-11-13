3763d74413d9c17f4dfa9d1283a3bbd542137e78
arseni-herasimovich <arseni.herasimovich@innowise.com>
2025-11-13 03:54:44 +0300

    init commit

---
+++ src/main/java/com/innowise/orderservice/InternshipOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipOrderServiceApplication.class, args);
+    }
+
+}
