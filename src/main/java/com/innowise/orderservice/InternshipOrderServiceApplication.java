6e020b4ae2c01bf43161a258a64bb3f9f9868255
arseni-herasimovich <arseni.herasimovich@innowise.com>
2025-11-19 03:45:42 +0300

    feat: main class settings

--- src/main/java/com/innowise/orderservice/InternshipOrderServiceApplication.java
+++ src/main/java/com/innowise/orderservice/InternshipOrderServiceApplication.java
@@ -2,8 +2,16 @@ package com.innowise.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+@EnableDiscoveryClient
+@EnableFeignClients
 public class InternshipOrderServiceApplication {
 
     public static void main(String[] args) {
