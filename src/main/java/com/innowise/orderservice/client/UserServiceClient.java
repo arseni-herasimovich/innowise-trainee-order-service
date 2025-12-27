88ae8c74522ee1377e2eaa155bd667501c32b71b
arseni-herasimovich <arseni.herasimovich@innowise.com>
2025-12-27 15:20:59 +0300

    feat: app settings unification

--- src/main/java/com/innowise/orderservice/client/UserServiceClient.java
+++ src/main/java/com/innowise/orderservice/client/UserServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", url = "${USER_SERVICE_URL:}")
 public interface UserServiceClient {
     @GetMapping("/api/v1/users/{id}")
     @CircuitBreaker(name = "user-service", fallbackMethod = "getUserByIdFallback")
